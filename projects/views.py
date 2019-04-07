b3c4e9a1d9f53c7e9cece3957d64e836f8bffa66
Andhika H. Santoso <andhika.haeruman@gmx.com>
2019-04-07 23:38:56 +0700

    Initial commit with showcase completed

---
+++ projects/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from projects.models import Project
+
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        'projects' : projects
+    }
+    return render(request, 'project_index.html', context)
+
+def project_detail(request, pk):
+    projects = Project.objects.get(pk=pk)
+    context = {
+        'project' : projects
+    }
+    return render(request, 'project_detail.html', context)
